85f4dea6b1f07a5d1b8cb1fc46bbf4ccbbc69f80
Hai-shuo Wang <Hai-Shuo.Wang@colorado.edu>
2026-06-27 14:03:00 +0200

    Delete AsterHoverVis directory

--- AsterHoverVis/smallbodyvis/core/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-@File        : smallbodyvis/core/__init__.py
-@Time        : 2026/06/27 13:47:07
-@Author      : Hai-Shuo Wang
-@Version     : 1.0
-@Contact     : wallen1732@gmail.com
-
-Core computation and data-loading modules for AsterHoverVis.
-'''
